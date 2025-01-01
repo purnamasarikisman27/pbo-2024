50fd9741352ea063a832ba8d607a641cc879a598
purnamasarikisman27 <sarikisman27@gmail.com>
2025-01-01 23:44:27 +0900

    Update main_pbo.py

--- main_pbo.py
+++ main_pbo.py
@@ -10,3 +10,17 @@ print ("\nTest Pesan : ", p.mes(), "\n")
 
 # silahkan uji fungsi dan class anda dibawah
 # pastikan class dan fungsi sudah anda buat di modul_satu_pbo.py
+
+#main_pbo.py
+from modul_satu_pbo import Mahasiswa, hitung_luas_persegi  # Mengimpor Mahasiswa dan hitung_luas_persegi
+
+if __name__ == "__main__":
+    # Uji class Mahasiswa
+    mhs = Mahasiswa("purnama sari ", "23075")
+    print(mhs.tampilkan_info())  # Menampilkan informasi mahasiswa
+
+    # Uji fungsi hitung_luas_persegi
+    panjang = 5
+    lebar = 3
+    Luas_Persegi_Panjang = hitung_luas_persegi(panjang, lebar)  # Menggunakan fungsi untuk menghitung luas
+    print(f"Luas Persegi Panjang: {Luas_Persegi_Panjang}")  # Menampilkan hasil luas
